f112fd0c5bd24f17cef4ea44110050a7e867418e
Jan van Hest <janvanhest@outlook.com>
2020-05-07 21:54:46 +0200

    added error handling to route api/users

--- routes/users.js
+++ routes/users.js
@@ -24,12 +24,28 @@ router.post(
     res.send("passed");
 
     const { name, email, password } = req.body;
-    try { 
-      let user = await User.findOne({email})
-      if(user) {
-        return res.status(400).json({msg: "User already exist"})
+    try {
+      let user = await User.findOne({ email });
+      if (user) {
+        return res.status(400).json({ msg: "User already exist" });
       }
-    } catch (err) { }
+
+      user = new User({
+        name,
+        email,
+        password,
+      });
+
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt)
+
+      await user.save();
+      res.send('User saved')
+    } catch (err) {
+      console.log(err.message)
+      res.status(500).send('Server error')
+    }
   }
 );
 
