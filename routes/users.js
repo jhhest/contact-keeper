2a55d58d35d39ad19819ec7a446fb7a9a05df066
Jan van Hest <janvanhest@outlook.com>
2020-05-15 23:11:46 +0200

    create user and respond with JSON webtoken.

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const { check, validationResult } = require("express-validator");
 const User = require("../models/User");
+const config = require("config");
 
 // @router  POST api/users
 // @desc    Register a user
@@ -22,9 +24,12 @@ router.post(
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
+
     const { name, email, password } = req.body;
+
     try {
       let user = await User.findOne({ email });
+
       if (user) {
         return res.status(400).json({ msg: "User already exist" });
       }
@@ -40,7 +45,24 @@ router.post(
       user.password = await bcrypt.hash(password, salt);
 
       await user.save();
-      res.send("User saved");
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        {
+          expiresIn: 3600,
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.log(err.message);
       res.status(500).send("Server error");
